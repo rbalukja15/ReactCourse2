cdcc679020983074c39e8d12793ae16ecb6a4fca
Romarjo Balukja <mbalukja1@gmail.com>
2020-12-04 19:28:57 +0100

    Leksioni 4 - Add validation of props using prop-types library

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
         <NavigationBar>
+            <Test name={"Name"} surname={"Surname"} age={20}/>
             <Test />
         </NavigationBar>
     </div>
--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Test(props) {
     return (
       <div>
-          <h1>Test Component </h1>
-          <p>The first test component</p>
+          <h1>{props.name}</h1>
+          <p>{props.surname}</p>
+          <p>{props.age}</p>
       </div>
     );
+}
+
+Test.propTypes = {
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string,
+    age: PropTypes.number
+}
+
+Test.defaultProps = {
+    name: "Default",
+    surname: "Default Surname",
+    age: 21
 }
 
 export default Test;
